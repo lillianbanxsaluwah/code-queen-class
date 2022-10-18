d18f1a45257105bda14316963eb9ab92e5ef8371
lillianbanxsaluwah <gwaidhosaluwahlillian@gmail.com>
2022-10-18 18:31:04 +0300

    my work

---
+++ Conditionals.js
@@ -0,0 +1,97 @@
+//this out puts our massage to the console
+//** */
+console.log('we have started')
+
+const age = document.getElementById('age').value;
+
+console.log(age);
+
+//let var or const
+
+
+
+
+let cones = 15;
+let barries ="15";
+let name = "LILLIAN";
+let weight= 55;
+
+/** 
+* SYNTAX
+*
+*first trial didnt work because of this
+* if (condition){executed when the condition is met}
+}
+* else{executed when the condition is not met}
+*/
+
+if(cones==15){
+    console.log("condition is met")
+}
+else{
+    console.log("condition not met")
+}
+if(name=="LILLIAN"){
+    console.log("condition met")
+}
+else{
+    console.log("condition not met")
+}
+if(barries){
+    console.log("condition met")
+}
+else{
+    console.log("condition not met")
+}
+
+if(weight< 0){
+    console.log ("You are weightless");
+}
+
+else if(weight > 0 && weight<=5){
+     console.log("You are in the range of 0 to 5");
+}
+else if("weight>5 && weight<=30"){
+    console.log("You are in the range of 6 to 30")
+
+}
+else if("weight> 30 && weight< 55"){
+    console.log("condition has been met");
+}
+else{
+     console.log ("You are really heavy");
+}
+
+
+//we are going to implement an elevator
+
+/**
+ * imlement an elevator
+ * 
+ * 
+ */
+let level = 1;
+
+if(level ==3){
+    console.log("The elevator is at level 3, its coming for you")
+}
+else if(level ==2){
+    console.log("The elevator is at level 2, coming for you")
+}
+else{
+    console.log("The elevator is at level 1, and its open to you")
+}
+
+// implementing swich including the operation of an elevator
+//** */
+Scanner scanner =new scanner (system.in);
+System.out.print("Enter a character:");
+char character=scanner.next().charAT(0);
+
+    switch (character){
+        case "A"
+                system.out.console("")
+        break;
+        case "B"
+
+    }
