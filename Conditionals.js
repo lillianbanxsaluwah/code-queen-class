9f42493d418661635965036f228da62d867d5657
lillianbanxsaluwah <gwaidhosaluwahlillian@gmail.com>
2022-10-19 13:07:52 +0300

    my work

--- Conditionals.js
+++ Conditionals.js
@@ -1,97 +1,150 @@
 //this out puts our massage to the console
-//** */
-console.log('we have started')
 
-const age = document.getElementById('age').value;
+// console.log('we have started')
 
-console.log(age);
+//  const age = document.getElementById('age').value;
 
-//let var or const
+// console.log(age);
 
 
+// //let var or const
+
+// //** 
+// let cones = 15;
+// let barries ="15";
+// let name = "LILLIAN";
+// let weight= 55;
+
+//***/
 
 
-let cones = 15;
-let barries ="15";
-let name = "LILLIAN";
-let weight= 55;
+// if(cones==15){
+//     console.log("condition is met")
+// }
+// else{
+//     console.log("condition not met")
+// }
+// if(name=="LILLIAN"){
+//     console.log("condition met")
+// }
+// else{
+//     console.log("condition not met")
+// }
+// if(barries){
+//     console.log("condition met")
+// }
+// else{
+//     console.log("condition not met")
+// }
 
-/** 
-* SYNTAX
-*
-*first trial didnt work because of this
-* if (condition){executed when the condition is met}
-}
-* else{executed when the condition is not met}
-*/
+// if(weight< 0){
+//     console.log ("You are weightless");
+// }
 
-if(cones==15){
-    console.log("condition is met")
-}
-else{
-    console.log("condition not met")
-}
-if(name=="LILLIAN"){
-    console.log("condition met")
-}
-else{
-    console.log("condition not met")
-}
-if(barries){
-    console.log("condition met")
-}
-else{
-    console.log("condition not met")
-}
+// else if(weight > 0 && weight<=5){
+//      console.log("You are in the range of 0 to 5");
+// }
+// else if("weight>5 && weight<=30"){
+//     console.log("You are in the range of 6 to 30")
 
-if(weight< 0){
-    console.log ("You are weightless");
-}
-
-else if(weight > 0 && weight<=5){
-     console.log("You are in the range of 0 to 5");
-}
-else if("weight>5 && weight<=30"){
-    console.log("You are in the range of 6 to 30")
-
-}
-else if("weight> 30 && weight< 55"){
-    console.log("condition has been met");
-}
-else{
-     console.log ("You are really heavy");
-}
+// }
+// else if("weight> 30 && weight< 55"){
+//     console.log("condition has been met");
+// }
+// else{
+//      console.log ("You are really heavy");
+// }
 
 
 //we are going to implement an elevator
 
-/**
- * imlement an elevator
- * 
- * 
- */
-let level = 1;
+//**
+ 
+// let level = 1;
 
-if(level ==3){
-    console.log("The elevator is at level 3, its coming for you")
-}
-else if(level ==2){
-    console.log("The elevator is at level 2, coming for you")
-}
-else{
-    console.log("The elevator is at level 1, and its open to you")
-}
+// if(level ==3){
+//     console.log("The elevator is at level 3 and its coming for you")
+// }
+// else if(level ==2){
+//     console.log("The elevator is at level 2 and coming for you")
+// }
+// else{
+//     console.log("The elevator is at level 1 and its open to you")
+// }
 
-// implementing swich including the operation of an elevator
-//** */
-Scanner scanner =new scanner (system.in);
-System.out.print("Enter a character:");
-char character=scanner.next().charAT(0);
 
-    switch (character){
-        case "A"
-                system.out.console("")
-        break;
-        case "B"
 
+// implementing switch including the operation of an elevator
+//SWITCH
+
+// const elevator = 1;
+// switch(elevator){
+
+//    case 5:
+//          console.log('Elevator  5 and its coming');
+//   break;
+
+//   case 4:
+//          console.log('Elevator 4 and itscoming');
+//   break;
+
+//   case 3:
+//          console.log(' Elevator 3 and its coming');
+//   break;
+   
+//   case 2:
+//          console.log('Elevator 2 the elevator is soon reaching ')
+
+//   case 1:
+//         console.log('Elevator has reached you at 1 and the doors are open');
+//   break;
+
+//   default:
+//        console.log('all will be ouk lets wait for  it');
+
+
+
+// }
+
+
+// for loop
+//      console.log("code:"+1)
+//      console.log("code:"+2)
+//      console.log("code:"+3)
+//      console.log("code:"+4)
+//      console.log("code:"+5)
+
+//     for(let i=5; i>=0; --i){
+//         if(i==3){
+//             break;
+//         }
+//         console.log("code:" + i)
+        
+//         }
+
+
+
+//     while loop
+//     let i=1
+//     while(i <= 5){
+//         console.log("code:"+i)
+//         i++
+//     }
+
+
+
+//     Array   *it stores a list of related values eg.names,numbers
+//     var names= [1,2,3,4];
+//     console.log(names[2])
+//     console.log(names[3])
+
+//     names[0] = 6
+//     console.log(names[0])
+
+
+
+    //For In *to iterate over arrays and objects
+    var names =["Purity","Lillian","V3ron"]
+    for(var i in names ){
+        console.log(names[i])
     }
